7be3ea4c43c629ffb12c06846273de8cfa46467d
Rui Liu <ruiliu@illinois.edu>
2016-05-25 15:12:37 -0500

    BD-1079 Added extractor registration code and info.

--- ocr/ocr.py
+++ ocr/ocr.py
@@ -15,11 +15,15 @@ from config import *
 import pyclowder.extractors as extractors
 
 def main():
-    global extractorName, messageType, rabbitmqExchange, rabbitmqURL    
+    global extractorName, messageType, rabbitmqExchange, rabbitmqURL, logger
 
     #set logging
     logging.basicConfig(format='%(asctime)-15s %(levelname)-7s : %(name)s -  %(message)s', level=logging.WARN)
-    logging.getLogger('pyclowder.extractors').setLevel(logging.DEBUG)
+    logging.getLogger('pyclowder.extractors').setLevel(logging.INFO)
+    logger = logging.getLogger('ocr')
+    logger.setLevel(logging.DEBUG)
+
+    register_extractor(clowderUrl, apiKey)
 
     #connect to rabbitmq
     extractors.connect_message_bus(extractorName=extractorName, messageType=messageType, processFileFunction=process_file, 
@@ -68,9 +72,32 @@ def process_file(parameters):
 
     extractors.upload_file_metadata(mdata=mdata, parameters=parameters)
 
+def register_extractor(host, key):
+    """Register extractor info with Clowder"""
 
+    logger.info("Registering extractor using key " + key)
+
+    headers = {'Content-Type': 'application/json'}
+
+    if not host.endswith("/"):
+        host += "/"
+
+    url = host+'api/extractors?key=' + key
+
+    info = {
+        "@context": "",
+        "name": extractorName,
+        "version": "1.0",
+        "description": "Simple OCR (Optical Character Recognition) extractor to extract text from an image",
+        "author": "Liana Diesendruck <ldiesend@illinois.edu>",
+        "contributors": ["Rob Kooper <kooper@illinois.edu>", "Rui Liu <ruiliu@illinois.edu>"],
+        "repository": {"repType": "git", "repUrl": "https://opensource.ncsa.illinois.edu/bitbucket/scm/cats/extractors-cv.git"},
+        "dependencies": ["tesseract"]
+    }
+
+    r = requests.post(url, headers=headers, data=json.dumps(info), verify=sslVerify)
+    print "Response: ", r.text
+    return r.raise_for_status()
 
 if __name__ == "__main__":
     main()
-
-
