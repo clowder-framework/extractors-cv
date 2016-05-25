fd9eced91a823113dc776cc5405390de502cfc93
Rui Liu <ruiliu@illinois.edu>
2016-05-25 14:29:52 -0500

    BD-1079 Changed config.py to use os.env(...).

--- ocr/config.py
+++ ocr/config.py
@@ -10,17 +10,19 @@
 #
 # =============================================================================
 
+import os
+
 # name to show in rabbitmq queue list
-extractorName = "ncsa.image.ocr"
+extractorName = os.getenv('RABBITMQ_QUEUE', "ncsa.image.ocr")
 
 # URL to be used for connecting to rabbitmq
-rabbitmqURL = None
+rabbitmqURL = os.getenv('RABBITMQ_URI', "amqp://guest:guest@localhost:5672/%2f")
 
 # name of rabbitmq exchange
-rabbitmqExchange = "medici"
+rabbitmqExchange = os.getenv('RABBITMQ_EXCHANGE', "clowder")
 
 # type of files to process
 messageType = "*.file.image.#"
 
 # trust certificates, set this to false for self signed certificates
-sslVerify=False
+sslVerify=os.getenv('RABBITMQ_SSLVERIFY', False)
