e38072da85d9867ae54e32626118b095dd14fdb4
Rui Liu <ruiliu@illinois.edu>
2016-06-22 15:34:02 -0500

    BD-1113 Updated config.py, profile.py, medici2-profiles.conf; added extractor info json, sample input/output files, Dockerfile and helper script.
    Tested. Docker container worked. On Mac OSX, maybe due to a diff opencv version from Ubuntu, did not detect the profile.

--- opencv/opencv-profiles/profile.py
+++ opencv/opencv-profiles/profile.py
@@ -1,14 +1,9 @@
 #!/usr/bin/env python
-import pika
-import sys
+
 import json
-import traceback
 import requests
 import tempfile
-import subprocess
 import os
-import itertools
-import numpy as np
 import cv2
 import time
 import logging
@@ -16,11 +11,18 @@ from config import *
 import pyclowder.extractors as extractors
 
 def main():
-    global extractorName, messageType, rabbitmqExchange, rabbitmqURL    
+    global extractorName, messageType, rabbitmqExchange, rabbitmqURL, logger
 
     #set logging
     logging.basicConfig(format='%(asctime)-15s %(levelname)-7s : %(name)s -  %(message)s', level=logging.WARN)
     logging.getLogger('pyclowder.extractors').setLevel(logging.DEBUG)
+    logger = logging.getLogger('profiles')
+    logger.setLevel(logging.DEBUG)
+
+    try:
+        register_extractor(registrationEndpoints)
+    except Exception as e:
+        logger.warn('Error in registering extractor: ' + str(e))
 
     #connect to rabbitmq
     extractors.connect_message_bus(extractorName=extractorName, messageType=messageType, processFileFunction=process_file, 
@@ -54,6 +56,7 @@ def process_file(parameters):
             (fd, sectionfile)=tempfile.mkstemp(suffix='.' + ext)
             os.close(fd)
 
+            positions=[]
             for (x,y,w,h) in faces:
                 roi_color = img_color[y:y+h, x:x+w]
                 cv2.imwrite(sectionfile, roi_color)
@@ -74,6 +77,14 @@ def process_file(parameters):
                 imgdata['height']=str(h)
                 imgdata['extractor_id']=extractorName
 
+                pos_md={}
+                pos_md['section_id']=sectionid
+                pos_md['x']=str(x)
+                pos_md['y']=str(y)
+                pos_md['width']=str(w)
+                pos_md['height']=str(h)
+                positions.append(pos_md)
+
                 #upload face as a section preview and associate metadata
                 extractors.upload_preview(previewfile=sectionfile, previewdata=imgdata, parameters=parameters)
 
@@ -92,15 +103,40 @@ def process_file(parameters):
                 mdata["extractor_id"]=extractorName                
                 extractors.upload_file_tags(tags=mdata, parameters=parameters)
 
+                # Add metadata.
+                # context url
+                context_url = 'https://clowder.ncsa.illinois.edu/clowder/contexts/metadata.jsonld'
+
+                # store results as metadata
+                metadata = {
+                    '@context': [context_url, 
+                                 {'profiles': 'http://clowder.ncsa.illinois.edu/' + extractorName + '#profiles'}],
+                    'attachedTo': {'resourceType': 'file', 'id': parameters["fileid"]},
+                    'agent': {'@type': 'cat:extractor',
+                              'extractor_id': 'https://clowder.ncsa.illinois.edu/clowder/api/extractors/' + extractorName},
+                    'content': {'profiles': positions}
+                }
+
+                # upload metadata
+                extractors.upload_file_metadata_jsonld(mdata=metadata, parameters=parameters)
+                logger.info("Uploaded metadata %s", metadata)
+
     finally:
         #os.remove(previewfile)
         if sectionfile is not None and os.path.isfile(sectionfile):     
             os.remove(sectionfile)  
 
+def register_extractor(registrationEndpoints):
+    """Register extractor info with Clowder"""
 
-
+    logger.info("Registering extractor...")
+    headers = {'Content-Type': 'application/json'}
+    with open('extractor_info.json') as info_file:
+        info = json.load(info_file)
+        info["name"] = extractorName
+        for url in registrationEndpoints.split(','):
+            r = requests.post(url.strip(), headers=headers, data=json.dumps(info), verify=sslVerify)
+            print "Response: ", r.text
 
 if __name__ == "__main__":
     main()
-
-
